fb76d4809c11a7d1cba08d86023caa9bb8960103
Rudresha <rudresha@Rudreshas-MacBook-Air.local>
2022-06-15 18:00:39 +0530

    fututre events

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -104,7 +104,7 @@ class EventsController extends BaseController
     public function getFutureEventsWithWorkshops(){
         $date= date('Y-m-d h:i:s');
         return Event::with('workshops')->whereHas('workshops',function($q) use($date){
-             $q->where('start','<=',$date);
+             $q->where('start','>=',$date);
          })->get();
     }
 
